3d35154c2f7f694cc076efd151f76636b5aea189
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-05-28 14:47:28 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ config.audible_bell = 'Disabled'
 config.color_scheme = 'tokyonight_storm'
 
 config.font = wezterm.font('OperatorMono Nerd Font', { weight = 400 })
-config.font_size = 19
+config.font_size = 20
 config.font_rules = {
     {
         intensity = 'Bold',
@@ -21,13 +21,21 @@ config.font_rules = {
     }
 }
 
+config.hide_tab_bar_if_only_one_tab = true
+
 config.line_height = 1.25
 
+config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE"
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
+    left = 8,
+    right = 8,
+    top = 8,
+    bottom = 8,
 }
+
+-- OS Specific
+config.macos_window_background_blur = 32
+
 
 return config
