53ce553bbf9f07ba71e93a23832e85bbe9dae19e
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-03-12 14:32:23 -0700

    feat: add pint and php-cs-fixer global configs

---
+++ .config/php-cs-fixer/php-cs-fixer.php
@@ -0,0 +1,25 @@
+<?php
+
+$rules = [
+    '@PSR12' => true,
+    'braces_position' => [
+        'anonymous_classes_opening_brace' => 'same_line',
+        'anonymous_functions_opening_brace' => 'same_line',
+        'classes_opening_brace' => 'same_line',
+        'control_structures_opening_brace' => 'same_line',
+        'functions_opening_brace' => 'same_line',
+    ],
+    'no_leading_import_slash' => false,
+    'not_operator_with_space' => false,
+    'not_operator_with_successor_space' => false,
+];
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRules($rules)
+    ->setFinder($finder)
+    ->setIndent('  ')
+    ->setLineEnding("\n")
+    ->setRiskyAllowed(true);
